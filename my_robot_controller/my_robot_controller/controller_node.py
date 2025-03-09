59667ecb466175717794d693040ccf0d63766351
basak-basak <basakucukbirinci@gmail.com>
2025-03-09 18:00:41 +0300

    Update controller_node.py

--- my_robot_controller/my_robot_controller/controller_node.py
+++ my_robot_controller/my_robot_controller/controller_node.py
@@ -2,37 +2,40 @@
 import rclpy 
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
+from nav_msgs.msg import Odometry
 
 class VelocityControllerNode(Node):
 
   def __init__(self):
      super().__init__("controller_node")
-     self.cmd_vel_publisher_= self.create_publisher(Twist,"/turtle1/cmd_vel",10)
-     self.pose_subscriber_ = self.create_subscription(Pose, "/turtle1/pose", self.pose_callback, 10)
+     self.cmd_vel_publisher_= self.create_publisher(Twist,"/cmd_vel",10)
+     self.pose_subscriber_ = self.create_subscription(Odometry, "/odom", self.pose_callback, 10)
      self.timer_ = self.create_timer(0.5, self.velocity_command)
      self.get_logger().info("Program has been started.")
 
   def velocity_command(self):
       cmd = Twist()      
       cmd.linear.x = 2.0
-      cmd.angular.z = 1.0
+      cmd.angular.z = 0.0
       self.cmd_vel_publisher_.publish(cmd) 
 
 
-  def pose_callback(self, pose: Pose):
+  def pose_callback(self, msg: Odometry):
+        
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
         cmd = Twist()   
 
-        if pose.x > 9.0 or pose.x < 2.0 or pose.y > 9.0 or pose.y < 2.0:
-            cmd.linear.x = 15.0
-            cmd.angular.z = 1.0
+        if x > 9.0 or x < 2.0 or y > 9.0 or y < 2.0:
+            cmd.linear.x = 1.0
+            cmd.angular.z = 0.5
         else:
-            cmd.linear.x = 20.0
-            cmd.angular.z = 0.0
+            cmd.linear.x = 3.0
+            cmd.angular.z = 0.5
             self.cmd_vel_publisher_.publish(cmd) 
         
  
-        if 0.5 <= pose.x <= 2.0 and 0.5 <= pose.y <= 2.0:
+        if 0.0 <= x <= 2.0 and 0.0 <= y <= 2.0:
           cmd.linear.x = 0.0
           cmd.angular.z = 0.0
           self.cmd_vel_publisher_.publish(cmd) 
